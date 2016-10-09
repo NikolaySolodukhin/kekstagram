d680e0c4ccddd9e762aa0491182132e2af8a5f53
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2016-10-09 11:37:22 +0300

    Сделал второе задание. Написал скрипт по обработке фотографий

---
+++ src/js/check.js
@@ -0,0 +1,26 @@
+function getMessage(a, b) {
+  var animationGif = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+  var noAnimationGif = "Переданное GIF-изображение не анимировано";
+  var svgPicture = "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+  if (typeof a === "number") {
+    return svgPicture;
+  } else if (typeof a === "boolean") {
+    if (a) {
+      return animationGif;
+    } else {
+      return noAnimationGif;
+    }
+  } else if (Array.isArray(a) && !Array.isArray(b)) {
+    var amountOfRedPoints = a.reduce(function(sum, current) {
+      return sum + current;
+    });
+    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = a.reduce(function(sum, current, i) {
+      return sum + (current * b[i]);
+    }, 0);
+    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+  } else {
+    return "Переданы некорректные данные";
+  }
+}
