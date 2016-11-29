aca970c87a5348cd4f144eef83bcda0ac3fee566
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2016-11-30 00:58:12 +0300

    Переписал модули из объектного стиля в использование прототипов

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -7,8 +7,11 @@ var Gallery = function() {
   this.activePicture = 0;
   this.allPictures = [];
   this.overlay = document.querySelector('.gallery-overlay');
-  this.overlayClose = document.querySelector('.gallery-overlay-close');
-  this.overlayImage = document.querySelector('.gallery-overlay-image');
+  this.overlayClose = this.overlay.querySelector('.gallery-overlay-close');
+  this.overlayImage = this.overlay.querySelector('.gallery-overlay-image');
+  this.hide = this.hide.bind(this);
+  this.show = this.show.bind(this);
+  this.onGalleryImageClick = this.onGalleryImageClick.bind(this);
 };
 
 Gallery.prototype.setPictures = function(arrayData) {
@@ -20,30 +23,25 @@ Gallery.prototype.clearPictures = function() {
 };
 
 Gallery.prototype.show = function(number) {
-  var self = this;
-  this.overlayClose.onclick = function() {
-    self.hide();
-  };
-  this.overlayImage.onclick = function() {
-    self.setActivePicture((self.activePicture + 1) % self.allPictures.length);
-  };
+  this.overlayClose.addEventListener('click', this.hide);
+  this.overlayImage.addEventListener('click', this.onGalleryImageClick);
   this.overlay.classList.remove('invisible');
   this.setActivePicture(number);
 };
-
+Gallery.prototype.onGalleryImageClick = function() {
+  this.setActivePicture((this.activePicture + 1) % this.allPictures.length);
+};
 Gallery.prototype.hide = function() {
   this.overlay.classList.add('invisible');
-  this.overlayClose.onclick = null;
-  this.overlayImage.onclick = null;
+  this.overlayClose.addEventListener('click', null);
+  this.overlayImage.addEventListener('click', null);
 };
 
 Gallery.prototype.setActivePicture = function(number) {
-  this.pictureLikes = this.overlay.querySelector('.likes-count');
-  this.pictureComments = this.overlay.querySelector('.comments-count');
   this.activePicture = number;
   this.overlayImage.src = this.allPictures[number].url;
-  this.pictureLikes.textContent = this.allPictures[number].likes;
-  this.pictureComments.textContent = this.allPictures[number].comments;
+  this.overlay.querySelector('.likes-count').textContent = this.allPictures[number].likes;
+  this.overlay.querySelector('.comments-count').textContent = this.allPictures[number].comments;
 };
 
 module.exports = new Gallery();
